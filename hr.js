cb5379e4005b8bdb478b8e6e59c5c35008ef11e6
Nyi Nyi Kyaw Thu <nyinyikyawthu79@gmail.com>
2026-07-03 20:49:47 +0630

    Implement HR page protection and logout functionality

--- hr.js
+++ hr.js
@@ -1,1 +1,11 @@
+import { protectHRPage, logoutUser } from "./firebase.js";
 
+protectHRPage();
+
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (logoutBtn) {
+  logoutBtn.addEventListener("click", () => {
+    logoutUser();
+  });
+}
